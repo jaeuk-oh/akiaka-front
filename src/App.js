1a3cb760d8d41b80c5408f05add60bc1e2162c0f
jaeuk oh <dds0410u@naver.com>
2024-07-21 18:46:23 +0900

    start akiaka

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> 리액트 프로젝트 시작.
         </p>
         <a
           className="App-link"
